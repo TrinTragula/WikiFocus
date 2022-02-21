6dc100a3d046ae2556603f5c3f673e27302e6ddf
TrinTragula <danielescarinci42@gmail.com>
2022-02-21 18:39:05 +0100

    Updated to manifest v3

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,9 +1,7 @@
-chrome.extension.sendMessage({}, function (response) {
-	var readyStateCheckInterval = setInterval(function () {
-		if (document.readyState === "complete") {
-			clearInterval(readyStateCheckInterval);
-			console.log("WikiFocus loaded")
-			document.getElementById("searchInput").focus();
-		}
-	}, 10);
-});
+const readyStateCheckInterval = setInterval(function () {
+	if (document.readyState === "complete") {
+		clearInterval(readyStateCheckInterval);
+		console.log("WikiFocus loaded")
+		document.getElementById("searchInput").focus();
+	}
+}, 10);
